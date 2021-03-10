ca644dd093051225ad2f3272771be917c2b8a5ab
jhubar <hubarjulien@gmail.com>
2021-03-10 11:39:31 +0100

    udpa

---
+++ js/Secu/secu.js
@@ -0,0 +1,15 @@
+const loginInputEmail = document.getElementById("InputEmail");
+const loginInputPassword = document.getElementById("InputPassword");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
